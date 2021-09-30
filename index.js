87817f42858fd1c050d0c3437e8965325efcfbf7
RalphChaves <ralphchaves@gmail.com>
2021-09-29 23:10:00 -0300

    up

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 app.set("view engine", "ejs"); //set engine para trabalhar com o EJS
 
 app.use(express.static(path.join(__dirname,"public"))); // set public como  pasta raiz para arquivo
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.render("index", {titulo: "Qualquer coisa"});
@@ -14,6 +15,33 @@ app.get("/", function (req, res) {
 app.get("/pagina", function (req, res) {
     res.render("pagina");
 });
+
+app.get("/formulario", function (req, res) {
+    res.render("formulario");
+
+});
+app.get("/cadastro", function (req, res) {
+    res.render("cadastro");
+
+});
+// app.post("/recebform", function (req, res) {
+//     const { nome, email, senha } = req.body;
+//     res.send({nome: nome, email: email, senha: senha});
+
+// });
+
+app.post("/formulario", function (req, res) {
+    const{nome, email, senha, endereco} = req.body;
+    const site = {nome: site.nome, senha: site.senha, email: site.email, endereco: site.endereco};
+    res.render("cadastro", site);
+})
+
+//Não é recomendado
+// app.get("/recebform", function (req, res) {
+//     const { nome, email, senha } = req.query;
+//     res.send({nome: nome, email: email});
+
+// });
 
 
 // app.get("/site", function (req, res) {
